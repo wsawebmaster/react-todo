0bd6c09f018e70897a87c2d671e9d88f2e47adcd
Wagner Andrade <wsawebmaster@yahoo.com.br>
2023-07-05 17:37:19 -0300

    first commit

---
+++ readme.md
@@ -0,0 +1,33 @@
+# 📂 Todo em React
+
+## 📃 Projeto desenvolvido conforme orientações do Matheus Battisti do canal Hora de Codar
+<br />
+
+## 🚀 Tecnologias
+
+- HTML
+- CSS
+- REACT
+<br />
+
+## 💻 Projeto
+> Frontend da aplicação 📸
+<p align="center">
+  <img alt="To-do list" src="./src/img/front.png" width="100%">
+</p>
+Instala Dependências
+
+    npm i
+
+  Executa projeto localmente
+
+    npm run dev
+
+<!-- [Acessar o projeto](https://wsawebmaster.github.io/react-todo/) -->
+
+<br />
+
+---
+---
+## 📧 Contato
+wsawebmaster@yahoo.com.br
---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+
+const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState("");
+  const [category, setCategory] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value || !category) return;
+    // console.log(value, category);
+    // adicionar todo
+    addTodo(value, category);
+    // limpar os campos
+    setValue("");
+    setCategory("");
+  };
+
+  return (
+    <div className='todo-form'>
+      <h2>Criar tarefa:</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Digite o título"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <select value={category} onChange={(e) => setCategory(e.target.value)}>
+            <option value="">Selecione uma categoria</option>
+            <option value="Trabalho">Trabalho</option>
+            <option value="Pessoal">Pessoal</option>
+            <option value="Estudos">Estudos</option>
+        </select>
+        <button type="submit">Criar tarefa</button>
+      </form>
+    </div>
+  )
+}
+
+export default TodoForm
